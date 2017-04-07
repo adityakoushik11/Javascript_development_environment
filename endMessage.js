09952c2f27bde38ff41c2a9230f92f872750d9c3
aditya <aditya.koushik11@gmail.com>
2017-04-07 17:31:40 +0530

    javascript dev environment

---
+++ endMessage.js
@@ -0,0 +1,2 @@
+var chalk=require("disp");
+console.log(chalk.yellow("ending the environment in development mode"));
---
+++ srcServer.js
@@ -0,0 +1,31 @@
+import express from "express";
+import path from "path";
+import open from "open";
+import webpack from "webpack";
+import config from "../webpack.config.dev";
+const port = 3005;
+const app = express();
+const compiler=webpack(config);
+app.use(require("webpack-dev-middleware") (compiler,{
+  noInfo:true,
+  publicPath:config.output.publicPath
+}
+));
+
+
+
+app.listen(port, function(err)
+{
+  if(err)
+  {
+    console.log("the error is : " +err);
+  }
+  else
+  {
+    open("http://localhost:" +port);
+  }
+});
+app.get("/", function(req,res)
+{
+  res.sendFile(path.join(__dirname,"../src/index.html"));
+});
